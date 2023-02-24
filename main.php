bb5725de8ea015de7a86e8a97cdb3eb935224de3
carlos98costa <46066793+carlos98costa@users.noreply.github.com>
2023-02-24 09:41:43 -0300

    Update main.php

--- main.php
+++ main.php
@@ -1,4 +1,17 @@
-<?php
-echo "The current date is ";
-echo date("l F d, Y");
-?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>002</title>
+</head>
+<body>
+    <h1>Exemplo PHP</h1>
+    <?php
+        date_default_timezone_set("America/Sao_Paulo"); 
+        print "Hoje é dia " . date("d/M/Y") ;
+        print " e a hora atual é " . date("G:i:s");
+    ?>
+</body>
+</html>
